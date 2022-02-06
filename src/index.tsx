9812d804d3f0cdad370259a2ea30c4960cd65253
sherlyzz <sherly_cs_sh@163.com>
2022-02-07 02:21:04 +0800

    Add: 状态组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,41 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 
-function tick() {
-	const element = (
-		<div>
-			<h1>Hello, world!</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
-		</div>
-	);
-	ReactDOM.render(element, document.getElementById('root'));}
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
 
-setInterval(tick, 1000);
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({date: new Date()});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Clock/>,
+	document.getElementById('root')
+);
 
 
 
